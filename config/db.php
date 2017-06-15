25b75a2f35b18ffe898a395cd9473e45d2fe9efa
Rostislav Dubrovin <dubrovin.rostislav@ya.ru>
2017-06-15 09:29:59 +0300

    Подключение БД

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=hh-php',
     'username' => 'root',
-    'password' => '',
+    'password' => '(/uv5eQi+>RtF\?',
     'charset' => 'utf8',
 ];
